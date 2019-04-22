ea5b121202444d8c67114ea6a6dd2a4e746048a0
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2019-04-22 15:15:57 -0700

    Add gulp automation tasks to project

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+( function() {
+
+'use strict';
+
+/* -------------------------------------------------------------------------------------------------
+** Variable Declarations
+*/
+
+// Gulp task dependencies
+var gulpBuilder = require( './WSU-UE---JS/gulpBuilder.js' );
+
+/* -------------------------------------------------------------------------------------------------
+** Function declarations
+*/
+
+/**
+ * Get the settings for a gulp-mediated custom CSS build from Less source files.
+ *
+ * @return {object} - Instance of gulpBuilder.CssBuildSettings.
+ */
+function getCssBuildSettings() {
+	var commentRemovalNeedle = /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm;
+	var dependenciesPath = './WSU-UE---CSS/';
+	var destFolder = './CSS/';
+	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css?family=Roboto+Mono:400,700|\
+Roboto+Condensed:400,700|Roboto+Slab|PT+Serif\');\r\n';
+	var insertingMediaQuerySectionHeader = {
+			'before': /^@media/,
+			'lineBefore': '/*! ╔═══════════════════════════════════════════════════════════════════\
+════════════════════════════════════════════════════╗\r\n*   ║ MEDIA QUERIES ######################\
+################################################################################# ║\r\n*   ╚═══════\
+═══════════════════════════════════════════════════════════════════════════════════════════════════\
+═════════════╝\r\n*/',
+			'stopAfterFirstMatch': true
+		};
+	var minCssFileExtension = '.min.css';
+	var minCssFileHeaderStr = '/*! Built with the Less CSS preprocessor [http://lesscss.org/]. Plea\
+se see [https://github.com/invokeImmediately/lsamp.wsu.edu] for a repository of source code. */\r\n\
+';
+ 	var sourceFile = './CSS/lsamp--legacy.less';
+
+	return new gulpBuilder.CssBuildSettings(commentRemovalNeedle, dependenciesPath,
+ 		destFolder, fontImportStr, insertingMediaQuerySectionHeader, minCssFileExtension,
+ 		minCssFileHeaderStr, sourceFile);
+}
+
+/**
+ * Get the settings for a gulp-mediated custom JS build.
+ *
+ * @return {object} - Simple collection of settings for JS builds.
+ */
+function getJsBuildSettings() {
+	return {
+		buildDependenciesList: [
+			'./JS/lsamp--legacy.js'
+		],
+		commentNeedle: /^(\/\*)(?!!)/g,
+		compiledJsFileName: 'lsamp.js',
+		destFolder: './JS/',
+		minJsFileExtension: '.min.js',
+		replaceCallback: gulpBuilder.fixFileHeaderComments
+	};
+}
+
+/* -------------------------------------------------------------------------------------------------
+** Main execution sequence
+*/
+
+gulpBuilder.setUpCssBuildTask( getCssBuildSettings() );
+gulpBuilder.setUpJsBuildTask( getJsBuildSettings() );
+
+} )();
