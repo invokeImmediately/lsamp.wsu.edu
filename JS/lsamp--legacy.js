4e4e6b1408b35917a94d11f060f0cca87c1dc293
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2019-07-30 22:13:46 -0700

    Update coding style to match guide

--- JS/lsamp--legacy.js
+++ JS/lsamp--legacy.js
@@ -1,9 +1,9 @@
-jQuery(function () {
+jQuery( function () {
     "use strict";
-    if (jQuery(".WSU_MAPS_NS").length) {
+    if ( jQuery(".WSU_MAPS_NS").length ) {
         setTimeout(function () { jQuery.wsu_maps.state.map_inst.setOptions({'zoom': 16}); }, 1000);
         jQuery(window).resize(function () {
             setTimeout(function () { jQuery.wsu_maps.state.map_inst.setOptions({'zoom': 16}); }, 1000);
-        });
+        } );
     }
-});
+} );
