a70b22d55b4eb566282cc4bd400184e0892e7075
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2020-03-05 09:26:16 -0800

    Updating custom JS build with recent submodule changes
    Rebuilding files containing custom JS for production usage to incorporate recent changes to submodules containing build dependencies universal to the websites of all DAESA programs and units.

--- JS/lsamp-build.js
+++ JS/lsamp-build.js
@@ -3287,3 +3287,51 @@ eed.';
 }
 
 } )( jQuery, 990, 198, 'auto-fits-text', '.column', 'jQuery.textResize.js' );
+
+/*!
+ * Site-specific JS for the WSU LSAMP Program.
+ *
+ * @author - Daniel Rieck ( danielcrieck@gmail.com ) [https://github.com/invokeImmediately]
+ */
+
+( function ( $ ) {
+
+"use strict";
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §1: Entry Point for Script Execution
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §1.1: Code executed after DOM is loaded
+
+$( function () {
+	// Tweak markup to resolve some issues from quirks of the WSU WordPress setup
+	checkForEventsCalendarPage();
+} );
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §2: Fixing WSUWP Markup
+
+// TODO: Add inline documentation
+function checkForEventsCalendarPage() {
+	var $body = $( 'body' );
+	if ( $body.hasClass( 'tribe-theme-spine' ) ) {
+		fixEventsCalendarHeader( $body );
+	}
+}
+
+// TODO: Add inline documentation
+function fixEventsCalendarHeader( $body ) {
+	var $main;
+	var $mainHeader;
+	var $subHeaderDefault;
+
+	if ( $.isJQueryObj( $body ) ) {
+		$main = $body.find( '#wsuwp-main' );
+		$mainHeader = $main.find( '.main-header' );
+		$subHeaderDefault = $mainHeader.find( '.sub-header-default' );
+		$subHeaderDefault.text( 'LSAMP: Louis Stokes Alliance for Minority Participation' );		
+	}
+}
+
+} )( jQuery );
